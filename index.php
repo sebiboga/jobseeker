a4f8ec47958f37c2739d7b33ca6b958a995e50d6
sebastianboga <sebastian.boga@endava.com>
2020-05-05 16:17:06 +0300

    first draft of home screen

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
+<html>
+<head>
+ <link rel="stylesheet"  type="txt/css" href="css/main.css" >
+
+</head>
+
+
 <?php
 title = "job seeker";
 
 ?>
+
+<body>
+
+<ul>
+    <li>Lorem ipsum dolor sit amet.</li>
+    <li>Non eveniet hic architecto quasi!</li>
+    <li>Porro doloremque ex, necessitatibus nesciunt.</li>
+    <li>Sequi sapiente molestiae a dolorum!</li>
+    <li>Nisi dignissimos porro culpa, enim.</li>
+</ul>
+</body>
+
+
+</html>
